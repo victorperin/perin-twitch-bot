4e97cd966fd24fa242705b94029b83e59e8a109c
Victor Perin <me@victorperin.ninja>
2020-08-11 22:28:34 -0300

    refactor(on-receive-messages/commands/telegram): removing type linkage

--- src/on-receive-messages/commands/telegram.ts
+++ src/on-receive-messages/commands/telegram.ts
@@ -1,6 +1,5 @@
-import { ChannelName } from "..";
 import { Chat } from "twitch-js";
 
-export default async (commandArguments: string[], chat: Chat, channel: ChannelName): Promise<void> => {
+export default async (commandArguments: string[], chat: Chat, channel: string): Promise<void> => {
     await chat.say(channel, 'https://t.me/perin_na_twitch')
 }
