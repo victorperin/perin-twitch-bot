6d18c845edc34f382f166757bd527b5d6d2f1b3f
Victor Perin <me@victorperin.ninja>
2020-08-12 17:59:28 -0300

    refactor(bot): use bot as a singleton instance to simplify architecture

--- src/run-bot.ts
+++ src/run-bot.ts
@@ -1,17 +1,8 @@
-import TwitchJs, { Chat } from 'twitch-js'
+import { Chat } from 'twitch-js'
 import onReceivePrivateMessages from './on-receive-messages'
+import { channel, chat } from './bot'
 
-const { TWITCH_BOT_TOKEN } = process.env
-
-const twitchAuth = {
-    username: 'victor_perin_bot',
-    token: TWITCH_BOT_TOKEN,
-    log: { level: 'error' },
-}
-
-const { chat } = new TwitchJs(twitchAuth)
-
-const runBot = async (channel: string): Promise<void> => {
+const runBot = async (): Promise<void> => {
     chat.on(Chat.Events.PRIVATE_MESSAGE, onReceivePrivateMessages(chat, channel))
 
     await chat.connect()
