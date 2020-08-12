cdf929883aa159745542c84c245b7db8c7c78812
Victor Perin <me@victorperin.ninja>
2020-08-11 21:07:23 -0300

    tests(e2e): basic test

---
+++ tests/e2e/telegram.test.ts
@@ -0,0 +1,42 @@
+import TwitchJs, { Chat } from 'twitch-js'
+import runBot, { disconnect } from '../../src/run-bot'
+import { promisify } from 'util'
+
+const sleep = promisify(setTimeout)
+
+const {
+    TWITCH_TEST_BOT_TOKEN,
+} = process.env
+
+const testBotOptions = {
+    username: 'cavaalo',
+    token: TWITCH_TEST_BOT_TOKEN,
+    log: { level: 'error' }
+}
+
+const BOT_CHANNEL = 'victor_perin_bot'
+
+
+it('should respond to !telegram', async () => {
+    const { chat: testBotChat } = new TwitchJs(testBotOptions)
+
+    await testBotChat.connect()
+    await testBotChat.join(BOT_CHANNEL)
+
+    await testBotChat.on(Chat.Events.PRIVATE_MESSAGE, (event) => {
+        expect(event.message).toBe('https://t.me/perin_na_twitch')
+    })
+
+    await runBot(BOT_CHANNEL)
+
+    await sleep(500)
+
+    await testBotChat.say(BOT_CHANNEL, '!telegram')
+    
+    await sleep(500)
+
+    testBotChat.disconnect()
+    disconnect()
+    expect.hasAssertions()
+
+})
