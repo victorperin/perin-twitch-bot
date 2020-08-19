850bb39e940ad338e7694cda1b66ae424675ca9d
Victor Perin <me@victorperin.ninja>
2020-08-19 13:59:49 -0300

    tests(interfaces/chatbot/chat): remove unnecessary descriptions

--- src/interfaces/chatbot/chat.test.ts
+++ src/interfaces/chatbot/chat.test.ts
@@ -7,21 +7,16 @@ const chatMocked = mocked(chatbot, true)
 
 beforeEach(jest.resetAllMocks)
 
-describe('chatbot', () => {
-    describe('chat', () => {
-        it('should aways be the same instance of chat element using node module cache', async () => {
-            const equalChatInstance = await import('./chat').then(x => x.chat)
+it('should aways be the same instance of chat element using node module cache', async () => {
+    const equalChatInstance = await import('./chat').then(x => x.chat)
 
-            jest.resetModuleRegistry()
-            const differentChatInstance = await import('./chat').then(x => x.chat)
+    jest.resetModuleRegistry()
+    const differentChatInstance = await import('./chat').then(x => x.chat)
 
-            expect(chatbot.chat).toStrictEqual(equalChatInstance)
-            expect(chatbot.chat).not.toStrictEqual(differentChatInstance)
-        })
+    expect(chatbot.chat).toStrictEqual(equalChatInstance)
+    expect(chatbot.chat).not.toStrictEqual(differentChatInstance)
+})
 
-        it('should be an Chat object', () => {
-            expect(chatbot.chat).toBeInstanceOf(Chat)
-        })
-    })
-    
+it('should be an Chat object', () => {
+    expect(chatbot.chat).toBeInstanceOf(Chat)
 })
