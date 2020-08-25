523e805af65c062f97c14e1cec6b0516c49d488c
Victor Perin <me@victorperin.ninja>
2020-08-25 05:27:13 -0300

    Proof of Concept: escolher nome do comando dentro do proprio arquivo do comando (#12)
    * refactor(on-receive-messages): command name defined inside own program
    
    * test(on-receive-messages)
    
    * refactor(commands/telegram): using structure to set the name of the command inside own module
    
    * feat(on-receive-messages): add command-importer
    
    * test(on-receive-messages): should do nothing if message does not start with !
    
    * refactor(on-receive-message): using new command importer module
    
    * chore(package): add ts-auto-mock package

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['jest-extended'],
+
+  globals: {
+    'ts-jest': {
+      compiler: "ttypescript",
+    },
+  },
 };
--- src/on-receive-messages/commands/telegram.ts
+++ src/on-receive-messages/commands/telegram.ts
@@ -2,8 +2,10 @@ import { chat, channel } from '../../interfaces/chatbot'
 
 export const TELEGRAM_CHANNEL_URL = 'https://t.me/perin_na_twitch'
 
-export default async (commandArguments: string): Promise<void> => {
+const telegramCommand = async (commandArguments: string): Promise<void> => {
     await chat.say(channel, TELEGRAM_CHANNEL_URL)
 }
 
-export const commandName = 'telegram'
+export const name = 'telegram'
+
+export default telegramCommand
