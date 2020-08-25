1fb6b06bbd410d25e7f787ded0e0049c2f0afa80
Victor Perin <me@victorperin.ninja>
2020-08-25 20:54:50 -0300

    perf(jest): only look at folders that contain tests and add verbose mode

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['jest-extended'],
+  testMatch: ['<rootDir>/tests/**/*.test.ts', '<rootDir>/src/**/*.test.ts'],
+  verbose: true,
 
   globals: {
     'ts-jest': {
