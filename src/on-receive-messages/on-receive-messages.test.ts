f9d64f5497f83eecb47da944b53d9ad13908c9da
Victor Perin <me@victorperin.ninja>
2020-08-25 04:25:28 -0300

    test(on-receive-messages)

---
+++ src/on-receive-messages/on-receive-messages.test.ts
@@ -0,0 +1,3 @@
+it.todo('should do nothing if message does not start with !')
+it.todo('should do nothing if message is bigger than 500 chars')
+it.todo('should do execute default function if no matches are found')
