e6c2f07c03c76ed33aa55c664dadd3edff754a31
Victor Perin <me@victorperin.ninja>
2020-08-25 05:21:01 -0300

    test(on-receive-messages): should do nothing if message does not start with !

--- src/on-receive-messages/on-receive-messages.test.ts
+++ src/on-receive-messages/on-receive-messages.test.ts
@@ -1,3 +1,21 @@
-it.todo('should do nothing if message does not start with !')
+import { PrivateMessages } from 'twitch-js'
+import { createMock } from 'ts-auto-mock';
+
+import onReceiveMessages from '.'
+
+
+it('should do nothing if message does not start with !', () => {
+    const substringSpy = jest.spyOn(String.prototype, 'substring')
+    const event = createMock<PrivateMessages>({ message: 'biruleibe'})
+
+    const response = onReceiveMessages(event)
+
+    expect(response).toStrictEqual(Promise.resolve(undefined))
+    expect(substringSpy).toBeCalledTimes(0)
+
+    substringSpy.mockRestore()
+})
+
+
 it.todo('should do nothing if message is bigger than 500 chars')
 it.todo('should do execute default function if no matches are found')
