ab914e2723191c6b88a75b65d23c81f755817c67
Victor Perin <me@victorperin.ninja>
2020-08-27 00:43:44 -0300

    test(chatbot/listeners): should listen to private messages

---
+++ src/interfaces/chatbot/listeners.test.ts
@@ -0,0 +1,20 @@
+import { mocked } from 'ts-jest/utils'
+
+import onReceivePrivateMessages from '../../on-receive-messages'
+jest.mock('../../on-receive-messages')
+const onReceivePrivateMessagesMocked = mocked(onReceivePrivateMessages)
+
+import { chat } from './chat'
+jest.mock('./chat')
+const chatMocked = mocked(chat, true)
+
+beforeEach(jest.resetAllMocks)
+
+import listeners from './listeners'
+
+it('should listen to private messages', () => {
+  listeners()
+
+  expect(chatMocked.on).toBeCalledWith('PRIVMSG', onReceivePrivateMessagesMocked)
+  expect(chatMocked.on).toBeCalledTimes(1)
+})
