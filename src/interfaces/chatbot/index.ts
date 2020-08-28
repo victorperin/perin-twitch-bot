3daf5f257938df69305fe175e2148599a006c1f2
Victor Perin <me@victorperin.ninja>
2020-08-28 05:19:07 -0300

    refactor(chatbot): remove chat object exposition

--- src/interfaces/chatbot/index.ts
+++ src/interfaces/chatbot/index.ts
@@ -1,12 +1,12 @@
-import { chat, channel, say } from './chat'
+import { channel, say } from './chat'
 import start from './start'
 import stop from './stop'
 
-export { chat, channel, say } from './chat'
+export { channel, say } from './chat'
 export { default as start } from './start'
 export { default as stop } from './stop'
 export default {
-  chat,
+  say,
   channel,
   stop,
   start,
