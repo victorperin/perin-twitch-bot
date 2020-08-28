93ebb855591d8051d9acfdc820b614eb7f9e257a
Victor Perin <me@victorperin.ninja>
2020-08-27 21:51:10 -0300

    test(on-receive-messages): ignore messages bigger than 500 chars

--- src/on-receive-messages/on-receive-messages.test.ts
+++ src/on-receive-messages/on-receive-messages.test.ts
@@ -1,5 +1,6 @@
 import { PrivateMessages } from 'twitch-js'
 import { createMock } from 'ts-auto-mock'
+import crypto from 'crypto'
 
 import onReceiveMessages from '.'
 
@@ -15,5 +16,17 @@ it('should do nothing if message does not start with !', () => {
   substringSpy.mockRestore()
 })
 
-it.todo('should do nothing if message is bigger than 500 chars')
+it('should do nothing if message is bigger than 500 chars', async () => {
+  const messageWithMoreThan500Chars = '!' + crypto.randomBytes(1000)
+
+  const substringSpy = jest.spyOn(String.prototype, 'substring')
+  const event = createMock<PrivateMessages>({ message: messageWithMoreThan500Chars })
+
+  await onReceiveMessages(event)
+
+  expect(substringSpy).toBeCalledTimes(0)
+
+  substringSpy.mockRestore()
+})
+
 it.todo('should do execute default function if no matches are found')
